9ed764a466b9ad00546cf8898dec509abc7e4435
Joaquinllopez00 <joaquinllopezzz@gmail.com>
2021-06-02 20:27:39 -0400

    pushing port

---
+++ app.js
@@ -0,0 +1,372 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+      }
+    });
+    burger.classList.toggle("toggle");
+  });
+};
+
+// const welcomeFadeIn = () => {
+//   const clouds = document.querySelector(".clouds");
+//   const welcome = document.querySelectorAll(".welcome");
+//   console.log(welcome);
+//   clouds.style.animation = `cloudsFadeIn 3s ease forwards `;
+//   setTimeout(() => {
+//     welcome.forEach((headerTag, index) => {
+//       if (headerTag.classList.contains("fullstack")) {
+//         headerTag.style.animation = ` fullstackFadeIn 3s ease forwards ${index / 5}s `;
+//       } else {
+//         headerTag.style.animation = `cloudsFadeIn 3s ease forwards ${index / 5}s `;
+//       }
+//     });
+//   }, 250);
+// };
+
+const welcomeFadeIn = () => {
+  const clouds = document.querySelector(".clouds");
+  const welcome = document.querySelectorAll(".welcome");
+  const firstname = document.querySelector(".firstname");
+  const backdrop = document.querySelector(".welcome-page__fadein");
+  const hello = document.querySelector(".welcome-message__firstSubcontainer");
+  const welcomeHello = document.querySelector(".hello");
+  const lastname = document.querySelector(".lastname");
+
+  const fullstack = welcome[3];
+  const developer = welcome[4];
+  const welcomeTL = gsap.timeline({ defaults: { duration: 0.7, ease: "power4.inOut" } });
+  welcomeTL.fromTo(backdrop, { opacity: 1 }, { opacity: 0, display: "none" }, "+=3");
+  welcomeTL.fromTo(clouds, { opacity: 0 }, { opacity: 1 }, "-=0.5");
+  welcomeTL.fromTo(hello, { rotationX: 80, rotationZ: 10, opacity: 0 }, { rotationX: 0, rotationZ: 0, opacity: "1" });
+  welcomeTL.fromTo(fullstack, { opacity: 0, x: -60, rotationY: -180 }, { opacity: 1, x: 0, rotationY: 0 });
+  welcomeTL.fromTo(
+    developer,
+    { opacity: 0, x: 60, rotationY: 180, rotationZ: 180 },
+    { opacity: 1, x: 0, rotationY: 0, rotationZ: 0 },
+    "-=1",
+  );
+  welcomeTL.fromTo(welcomeHello, { opacity: 1, x: 0 }, { opacity: 0 }, "+=1");
+
+  if (window.innerWidth < 400) {
+    welcomeTL.fromTo(firstname, { marginLeft: "0" }, { marginLeft: "155px" }, "-=0.8");
+  }
+  welcomeTL.fromTo(
+    lastname,
+    { display: "none", opacity: 0, rotationX: -180 },
+    { display: "block", rotationX: 0, opacity: 1 },
+    "-=1.5",
+  );
+  welcomeTL.fromTo(hello, { x: 0 }, { x: -235 }, "-=1");
+};
+
+const buttonTransform = () => {
+  const button = document.querySelector(".visit-page");
+  const glow = document.querySelector(".glow");
+  button.addEventListener("mouseover", () => {
+    button.style.animation = `buttonHover 0.3s ease forwards`;
+    button.classList.toggle("toggled");
+    glow.classList.toggle("glowToggle");
+  });
+  button.addEventListener("mouseout", () => {
+    button.style.animation = `buttonUnhover 0.3s ease forwards`;
+    button.classList.toggle("toggled");
+    glow.classList.toggle("glowToggle");
+  });
+};
+
+const buttonOnClick = () => {
+  const button = document.querySelector(".visit-page");
+  button.addEventListener("click", () => {
+    const nav = document.querySelector("nav").offsetTop;
+    const verticalNav = document.querySelector(".vertical-nav");
+    window.scrollTo({
+      top: `${nav}`,
+      left: 0,
+      behavior: "smooth",
+    });
+    setTimeout(() => {
+      verticalNav.classList.toggle("vertical-nav__toggled");
+    }, 500);
+    // setTimeout(() => {
+
+    // }, 420);
+  });
+};
+
+const body = document.querySelector("body");
+
+//vertical nav
+const vertNav = document.querySelector(".vertical-nav");
+const nav = document.querySelector("nav");
+//section 1
+const about = document.querySelector("#about");
+const aboutUnderline = document.querySelector(".about__underline");
+const aboutImage = document.querySelector(".about-content__image");
+const aboutHello = document.querySelector("#about-hello");
+const aboutContent = document.querySelector("#about-hello__content");
+const vertLine = document.querySelector("#about__vertical-line");
+const horzLine = document.querySelector("#about__horizontal-line");
+const hover = document.querySelector("#hover");
+let x = 0;
+//port intro section
+const portfolio = document.querySelector("#projects");
+const portfolioUnderline = document.querySelector(".projects__underline");
+const techSkills = document.querySelector("#technical");
+const skills = document.querySelectorAll(".projects-skills-languages");
+let y = 0;
+
+// project section
+const horzLineBreak = document.querySelector(".horizontal-line-break");
+const project = document.querySelector(".projects-projects__container");
+const projectsHeader = document.querySelector("#projects-header");
+const projectSections = document.querySelectorAll(".projects__container");
+const gifLeft = document.querySelectorAll(".projects-gif__left");
+const gifRight = document.querySelectorAll(".projects-gif__right");
+const projectAboutRight = document.querySelectorAll(".projects-about__right");
+const projectAboutLeft = document.querySelectorAll(".projects-about__left");
+let z = 0;
+//second section
+let j = 0;
+//third
+let k = 0;
+//fourth
+let l = 0;
+scroll = () => {
+  const section = document.querySelectorAll("section");
+
+  console.log(projectSections[3].offsetTop - window.scrollY);
+  // console.log(projectSections[1]);
+  if (section[0].offsetTop - window.scrollY < 200 && x === 0) {
+    gsap.fromTo(vertNav, { x: -20, opacity: 0 }, { x: 0, opacity: 1 });
+    aboutTL = gsap.timeline({ defaults: { duration: 0.8, ease: "power3.inOut" } });
+    aboutTL.fromTo(about, { x: -200, opacity: 0 }, { x: 0, opacity: 1 });
+    aboutTL.fromTo(aboutUnderline, { x: +200, opacity: 0 }, { x: 0, opacity: 1 }, "-=0.8");
+    aboutTL.fromTo(aboutImage, { x: -200, rotationZ: -20, opacity: 0 }, { rotationZ: 0, x: 0, opacity: 1 }, "-=0.5");
+    aboutTL.fromTo(aboutHello, { x: +200, opacity: 0 }, { x: 0, opacity: 1 }, "-=0.3");
+    aboutTL.fromTo(aboutContent, { x: +200, opacity: 0 }, { x: 0, opacity: 1 }, "-=0.7");
+    aboutTL.fromTo(aboutContent, { x: +200, opacity: 0 }, { x: 0, opacity: 1 }, "-=0.7");
+    aboutTL.fromTo(vertLine, { width: 20, opacity: 0 }, { width: 200, opacity: 1 }, "-=0.7");
+    aboutTL.fromTo(horzLine, { width: 20, opacity: 0 }, { width: 200, opacity: 1 }, "-=0.7");
+    x += 1;
+  } else if (section[0].offsetTop - window.scrollY > 500 && x === 1) {
+    gsap.to(vertNav, { x: -20, opacity: 0 });
+    gsap.fromTo(about, { x: 0, opacity: 1 }, { x: -200, opacity: 0 });
+    gsap.fromTo(aboutUnderline, { x: 0, opacity: 1 }, { x: +200, opacity: 0 }, "-=0.8");
+    gsap.fromTo(aboutImage, { rotationZ: 0, x: 0, opacity: 1 }, { x: -200, rotationZ: -20, opacity: 0 });
+    gsap.fromTo(aboutHello, { x: 0, opacity: 1 }, { x: +200, opacity: 0 });
+    gsap.fromTo(aboutContent, { x: 0, opacity: 1 }, { x: +200, opacity: 0 });
+    gsap.fromTo(aboutContent, { x: 0, opacity: 1 }, { x: +200, opacity: 0 });
+    gsap.fromTo(vertLine, { width: 200, opacity: 1 }, { width: 20, opacity: 0 });
+    gsap.fromTo(horzLine, { width: 200, opacity: 1 }, { width: 20, opacity: 0 });
+    x = 0;
+  } else if (section[1].offsetTop - window.scrollY < 400 && y === 0) {
+    y += 1;
+    portTL = gsap.timeline({ defaults: { duration: 0.7, ease: "power3.inOut" } });
+    portTL.fromTo(portfolio, { x: -200, opacity: 0 }, { x: 0, opacity: 1 });
+    portTL.fromTo(portfolioUnderline, { x: +200, opacity: 0 }, { x: 0, opacity: 1 }, "-=0.8");
+    portTL.fromTo(techSkills, { y: -10, opacity: 0 }, { y: 0, opacity: 1 }, "-=0.6");
+    portTL.fromTo(skills[0], { y: +10, opacity: 0 }, { y: 0, opacity: 1 }, "-=0.6");
+    portTL.fromTo(skills[1], { y: +10, opacity: 0 }, { y: 0, opacity: 1 }, "-=0.5");
+    portTL.fromTo(skills[2], { y: +10, opacity: 0 }, { y: 0, opacity: 1 }, "-=0.4");
+  } else if (project.offsetTop - window.scrollY < 400 && z === 0) {
+    let projectAboutR = projectAboutRight[0].children[0].children;
+    firstTL = gsap.timeline({ defaults: { duration: 0.7, ease: "power3.inOut" } });
+    z += 1;
+    console.log(projectsHeader);
+    firstTL.fromTo(horzLineBreak, { y: 20, opacity: 0 }, { y: 0, opacity: 1 });
+    firstTL.fromTo(projectsHeader, { y: 20, opacity: 0 }, { y: 0, opacity: 1 }, "-=0.6");
+    firstTL.fromTo(gifLeft[0], { x: -200, opacity: 0 }, { x: 0, opacity: 1 }, "-=0.5");
+    firstTL.fromTo(projectAboutR[0], { x: 200, opacity: 0 }, { x: 0, opacity: 1 }, "-=0.4");
+    firstTL.fromTo(projectAboutR[1], { x: 200, opacity: 0 }, { x: 0, opacity: 1 }, "-=0.35");
+    firstTL.fromTo(projectAboutR[2].children[0], { y: 200, opacity: 0 }, { y: 0, opacity: 1 }, "-=0.3");
+    firstTL.fromTo(projectAboutR[2].children[1], { y: 200, opacity: 0 }, { y: 0, opacity: 1 }, "-=0.3");
+  } else if (projectSections[1].offsetTop - window.scrollY < -2300 && j === 0) {
+    let projectAboutL = projectAboutLeft[0].children[0].children;
+    secondTL = gsap.timeline({ defaults: { duration: 0.7, ease: "power3.inOut" } });
+    j += 1;
+    secondTL.fromTo(gifRight[0], { x: 100, opacity: 0 }, { x: 0, opacity: 1 });
+    secondTL.fromTo(projectAboutL[0], { x: -200, opacity: 0 }, { x: 0, opacity: 1 }, "-=0.4");
+    secondTL.fromTo(projectAboutL[1], { x: -200, opacity: 0 }, { x: 0, opacity: 1 }, "-=0.35");
+    secondTL.fromTo(projectAboutL[2].children[0], { y: 200, opacity: 0 }, { y: 0, opacity: 1 }, "-=0.3");
+    secondTL.fromTo(projectAboutL[2].children[1], { y: 200, opacity: 0 }, { y: 0, opacity: 1 }, "-=0.3");
+  } else if (projectSections[2].offsetTop - window.scrollY < -2775 && k === 0) {
+    console.log("now");
+    let projectAboutR = projectAboutRight[1].children[0].children;
+    console.log(projectAboutR);
+    thirdTL = gsap.timeline({ defaults: { duration: 0.7, ease: "power3.inOut" } });
+    k += 1;
+    thirdTL.fromTo(gifLeft[1], { x: -200, opacity: 0 }, { x: 0, opacity: 1 });
+    thirdTL.fromTo(projectAboutR[0], { x: 200, opacity: 0 }, { x: 0, opacity: 1 }, "-=0.4");
+    thirdTL.fromTo(projectAboutR[1], { x: 200, opacity: 0 }, { x: 0, opacity: 1 }, "-=0.35");
+    thirdTL.fromTo(projectAboutR[2].children[0], { y: 200, opacity: 0 }, { y: 0, opacity: 1 }, "-=0.3");
+    thirdTL.fromTo(projectAboutR[2].children[1], { y: 200, opacity: 0 }, { y: 0, opacity: 1 }, "-=0.3");
+  } else if (projectSections[3].offsetTop - window.scrollY < -3400 && l === 0) {
+    let projectAboutL = projectAboutLeft[1].children[0].children;
+    fourthTL = gsap.timeline({ defaults: { duration: 0.7, ease: "power3.inOut" } });
+    l += 1;
+    fourthTL.fromTo(gifRight[1], { x: 100, opacity: 0 }, { x: 0, opacity: 1 });
+    fourthTL.fromTo(projectAboutL[0], { x: -200, opacity: 0 }, { x: 0, opacity: 1 }, "-=0.4");
+    fourthTL.fromTo(projectAboutL[1], { x: -200, opacity: 0 }, { x: 0, opacity: 1 }, "-=0.35");
+    fourthTL.fromTo(projectAboutL[2].children[0], { y: 200, opacity: 0 }, { y: 0, opacity: 1 }, "-=0.3");
+    fourthTL.fromTo(projectAboutL[2].children[1], { y: 200, opacity: 0 }, { y: 0, opacity: 1 }, "-=0.3");
+  } else if (section[1].offsetTop - window.scrollY > 430 && y === 1) {
+    y = z = j = k = l = 0;
+    let projectAboutR = projectAboutRight[0].children[0].children;
+    let projectAboutL = projectAboutLeft[0].children[0].children;
+    let projectAboutR1 = projectAboutRight[1].children[0].children;
+    let projectAboutL1 = projectAboutLeft[1].children[0].children;
+    console.log(y);
+    gsap.to(portfolio, { x: -200, opacity: 0 });
+    gsap.to(portfolioUnderline, { x: +200, opacity: 0 });
+    gsap.to(techSkills, { y: -10, opacity: 0 });
+    gsap.to(skills[0], { y: +10, opacity: 0 });
+    gsap.to(skills[1], { y: +10, opacity: 0 });
+    gsap.to(skills[2], { y: +10, opacity: 0 });
+    gsap.to(gifLeft[0], { x: -100, opacity: 0 });
+    gsap.to(projectsHeader, { y: 20, opacity: 0 });
+    gsap.to(horzLineBreak, { y: 20, opacity: 0 });
+    gsap.to(projectAboutR[0], { x: 200, opacity: 0 });
+    gsap.to(projectAboutR[1], { x: 200, opacity: 0 });
+    gsap.to(projectAboutR[2].children[0], { y: 200, opacity: 0 });
+    gsap.to(projectAboutR[2].children[1], { y: 200, opacity: 0 });
+    gsap.to(gifRight[0], { x: 100, opacity: 0 });
+    gsap.to(projectAboutL[0], { x: -200, opacity: 0 });
+    gsap.to(projectAboutL[1], { x: -200, opacity: 0 });
+    gsap.to(projectAboutL[2].children[0], { y: 200, opacity: 0 });
+    gsap.to(projectAboutL[2].children[1], { y: 200, opacity: 0 });
+    gsap.to(gifLeft[1], { x: -100, opacity: 0 });
+    gsap.to(projectAboutR1[0], { x: 200, opacity: 0 });
+    gsap.to(projectAboutR1[1], { x: 200, opacity: 0 });
+    gsap.to(projectAboutR1[2].children[0], { y: 200, opacity: 0 });
+    gsap.to(projectAboutR1[2].children[1], { y: 200, opacity: 0 });
+    gsap.to(gifRight[1], { x: 100, opacity: 0 });
+    gsap.to(projectAboutL1[0], { x: -200, opacity: 0 });
+    gsap.to(projectAboutL1[1], { x: -200, opacity: 0 });
+    gsap.to(projectAboutL1[2].children[0], { y: 200, opacity: 0 });
+    gsap.to(projectAboutL1[2].children[1], { y: 200, opacity: 0 });
+  }
+};
+
+window.addEventListener("scroll", () => scroll());
+aboutImage.addEventListener("mouseenter", () => {
+  gsap.to(vertLine, { width: 180, opacity: 0 });
+  gsap.to(horzLine, { width: 180, top: -30, opacity: 0 });
+  if (window.innerWidth > 1000) {
+    gsap.to(aboutImage, { boxShadow: "-20px -20px 25px #ee1d52" });
+    gsap.to(hover, { opacity: 0 });
+  }
+});
+aboutImage.addEventListener("mouseleave", () => {
+  gsap.to(vertLine, { width: 200, opacity: 1 });
+  gsap.to(horzLine, { width: 200, top: -20, opacity: 1 });
+  if (window.innerWidth > 1000) {
+    gsap.to(aboutImage, { boxShadow: "0px 0px 0px #ee1d52" });
+    gsap.to(hover, { opacity: 1 });
+  }
+});
+
+const loadingGif = () => {
+  const gif = document.querySelector("iframe");
+  // body.style.overflow = "hidden";
+  setTimeout(() => {
+    body.style.overflow = "visible";
+    gsap.to(gif, { opacity: 0, scale: 2 });
+  }, 3000);
+};
+
+// const contact = (e) => {
+//   e.preventDefault();
+
+// };
+
+window.onload = function () {
+  document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    // generate a five digit number for the contact_number variable
+    // this.contact_number.value = (Math.random() * 100000) | 0;
+    // these IDs from the previous steps
+    const host = document.querySelector("#user_name").value;
+    const emailAdd = document.querySelector("#user_email").value;
+    const contents = document.querySelector("#message").value;
+    const mess = document.getElementById("submit-message");
+    console.log(host, emailAdd, contents);
+    emailjs
+      .send("service_97k0vwi", "template_yvwn842", {
+        from_name: host,
+        to_name: "Joaquin",
+        message: contents,
+        reply_to: emailAdd,
+        from_email: emailAdd,
+      })
+      .then(
+        function () {
+          mess.style.color = "#1dee7b";
+          mess.innerText = "Success! I'll be getting back to you shortly!";
+          setTimeout(() => {
+            mess.innerText = "";
+          }, 3000);
+        },
+        function (error) {
+          mess.style.color = "#ee1d52";
+          mess.innerText = "Something broke, email me at joaquinllopezzz@gmail.com";
+        },
+      );
+  });
+};
+
+const navLink = (link) => {
+  let x;
+  switch (link) {
+    case "about":
+      x = document.getElementById("about").offsetTop;
+      window.scrollTo({
+        top: x - 150,
+        left: 0,
+        behavior: "smooth",
+      });
+      break;
+    case "projects":
+      x = document.getElementById("projects").offsetTop;
+      window.scrollTo({
+        top: x - 150,
+        left: 0,
+        behavior: "smooth",
+      });
+      break;
+    case "contact":
+      x = document.getElementById("contact").offsetTop;
+      window.scrollTo({
+        top: x - 150,
+        left: 0,
+        behavior: "smooth",
+      });
+      break;
+  }
+};
+
+// 51A403893E1ECC165A59DA91CF2138989C34AC1C462C69D220CCCCF05A864AAF0A9E73813A208D9FC5FCF048704818FB
+
+// window.onbeforeunload = function () {
+//   window.scrollTo(0, 0);
+// };
+
+const App = () => {
+  loadingGif();
+  navSlide();
+  welcomeFadeIn();
+  buttonTransform();
+  buttonOnClick();
+  (function () {
+    emailjs.init("user_9isBXwaZumJLmdq2e7bFo");
+  })();
+};
+
+App();
